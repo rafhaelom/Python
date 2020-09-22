6ae171ad7fcf247ca3065f50e56f052b793565f6
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2020-09-22 08:07:12 -0300

    Adicionando arquivos .py

---
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/2.py
@@ -0,0 +1,5 @@
+# 2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+print("Digite um número: ")
+numero = input()
+print("O número informado foi [", numero, "]")
