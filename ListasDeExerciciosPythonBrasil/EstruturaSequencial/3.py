4af5878e2adb605d24b90059c2a3df7971aba218
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2020-09-24 22:40:49 -0300

    Adicionando arquivo .py

---
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/3.py
@@ -0,0 +1,13 @@
+# 3. Faça um Programa que peça dois números e imprima a soma.
+
+# Soma de números do tipo Inteiro
+print("Informe dois números Inteiros!")
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+print("A soma dos números inteiros é: ", (a + b))
+
+# Soma de números do tipo Float
+print("\n\nInforme dois números Float!")
+a = float(input("Digite um número: "))
+b = float(input("Digite outro número: "))
+print("A soma dos números float é: ", (a + b))
