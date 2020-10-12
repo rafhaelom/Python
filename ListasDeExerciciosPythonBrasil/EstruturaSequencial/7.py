399d0fb4a20c5b7ba3c94ef9f9e383e1eff7fe23
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2020-10-12 20:41:47 -0300

    Adicionando arquivo .py

---
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/7.py
@@ -0,0 +1,6 @@
+# 7. Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+base = int(input("Digite a base do quadrado: "))
+altura = int(input("Digite a altura do quadrado: "))
+area = base * altura
+print("O dobro da area do quadrado eh: ", area ** 2)
