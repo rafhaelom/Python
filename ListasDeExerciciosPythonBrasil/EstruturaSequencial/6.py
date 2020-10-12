59978069a2664d39dae235df7f2174e89facce7e
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2020-10-12 20:00:50 -0300

    Adicionando arquivo .py

---
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/6.py
@@ -0,0 +1,4 @@
+# 6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = float(input("Digite o raio de um circulo: "))
+print("A area do circulo eh: ", 3.14 * (raio ** 2))
