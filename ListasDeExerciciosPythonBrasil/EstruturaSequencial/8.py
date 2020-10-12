8e72c2253059eb8ab99e333f69e4765066a694d1
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2020-10-12 20:51:06 -0300

    Adicionando arquivo .py

---
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/8.py
@@ -0,0 +1,7 @@
+# 8. Faça um Programa que pergunte quanto você ganha por hora e o
+# número de horas trabalhadas no mês. Calcule e mostre o total do
+# seu salário no referido mês.
+
+ganhaHr = float(input("Quanto ganha por hora: "))
+numHr = float(input("Quantas horas trabalhou por mes: "))
+print("Seu salario no final do mes eh: ", ganhaHr * numHr)
