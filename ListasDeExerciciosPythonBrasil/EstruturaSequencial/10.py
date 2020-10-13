373b6473c6f37efad91c75eb9edfc0faeed8ef22
Rafhael de Oliveira Martins <67989242+rafhaelom@users.noreply.github.com>
2020-10-12 21:26:30 -0300

    Adicionando arquivo .py

---
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/10.py
@@ -0,0 +1,3 @@
+# 10. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+temCelsius = float(input("Digite a temperatura em Celsius: "))
+print("A temperatura em Fahrenheit eh: ", (1.8 * temCelsius) + 32, "ºF")
