172ac8c1701f53b78e618b166ce61716d73bb055
rafhaelom <rafha-1012@hotmail.com>
2021-02-19 16:44:02 -0300

    :rocket: update: script refactor

--- ListasDeExerciciosPythonBrasil/EstruturaSequencial/16.py
+++ ListasDeExerciciosPythonBrasil/EstruturaSequencial/16.py
@@ -3,14 +3,14 @@
 # e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades 
 # de latas de tinta a serem compradas e o preço total.
 
-cobertura_m = 3
+cobertura = 3
 capacidade_lata = 18
-custo_lata = 80.00
+custo_lata = 80
 
 tamanho = int(input("Qual o tamanho da área a ser pintada em metros quadrados: "))
 
-quantidade_l = tamanho / cobertura_m
-latas = int(quantidade_l / capacidade_lata)
+quantidade_litro = tamanho / cobertura
+latas = int(quantidade_litro / capacidade_lata)
 
 print(f"Precisará de {latas} latas de tintas de 18 litros.")
 print(f"Total ficará em: R$ {latas * custo_lata}")
